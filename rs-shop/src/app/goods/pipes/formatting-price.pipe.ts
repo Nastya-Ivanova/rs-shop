cf2a0fedfe3c2abf17ae4faffd62ee12324cd80e
Nastya-Ivanova <tkacheva123@gmail.com>
2021-09-18 19:53:10 +0300

    fix: refactor pipe formatting price

--- rs-shop/src/app/goods/pipes/formatting-price.pipe.ts
+++ rs-shop/src/app/goods/pipes/formatting-price.pipe.ts
@@ -5,6 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormattingPricePipe implements PipeTransform {
   transform(price: number): string {
+    if (`${price}`.length - `${price}`.indexOf('.') === 2) {
+      return `${price.toLocaleString('ru-RU')}0`;
+    }
+    if (`${price}`.indexOf('.')) {
+      return price.toLocaleString('ru-RU');
+    }
     return `${price.toLocaleString('ru-RU')},00`;
   }
 }
